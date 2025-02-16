61f02ff8140d0858fcc29823d3399a566f90cc9c
Juan C. Solar Torres <stechware2023@gmail.com>
2025-02-16 11:04:22 -0500

    Inicializar Repositorio

---
+++ test/src/main/java/com/idat/test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.idat.test;
+
+/**
+ *
+ * @author solaris
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
